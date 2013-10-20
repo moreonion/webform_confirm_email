4b05e48761f494ec266214282788b5555aa3abee
Matthias Weiss <matthias@more-onion.com>
2013-10-20 22:26:19 +0200

    * Implemented a "confirmation request expired" rules event * Implemented a "confirmation request expired" hook

--- webform_confirm_email.api.php
+++ webform_confirm_email.api.php
@@ -12,15 +12,33 @@
  * the confirmation link
  *
  * @param $node
- *   The node containing the webform where a email was confirmed
+ *   The node object of the webform for which an email was confirmed
  *
  * @param $submission
- *   The original submission of the user that just confirmed his/her
- *   email address
+ *   The submission object of the webform submission where the user
+ *   just confirmed his/her email address
  */
 function hook_email_confirmed($node, $submission) {
   db_query(
     'INSERT INTO {my_confirmed_submission_list} ' .
     '  VALUES (:nid, :sid) ',
-    array(':nid' => $node->nid, ':sid' => $submission->sid);
+    array(':nid' => $node->nid, ':sid' => $submission->sid)
+  );
+}
+
+/**
+ * React on an email confirmation request that has expired according
+ * to the maximum request lifetime that the admin had set
+ *
+ * @param $node
+ *   The node object of the webform for which an email confirmation
+ *   request just expired
+ *
+ * @param $submission
+ *   The submission object of the webform submission for which the
+ *   email confirmation request just exirted
+ */
+function hook_confirmation_request_expired($node, $submission) {
+  // delete the webform submission that was not confirmed
+  webform_submission_delete($node, $submission);
 }
