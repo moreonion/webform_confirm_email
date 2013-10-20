51e60fc2ed9c86d941792bb5882f4cb4fc105382
Matthias Weiss <matthias@more-onion.com>
2013-10-20 22:48:59 +0200

    * changed "confirmation request expired" hook as the previous   implementation would probably run into performance problems when expiring   many submissions * removed "confirmation request expired" rules event because of the same   performance problems

--- webform_confirm_email.api.php
+++ webform_confirm_email.api.php
@@ -30,15 +30,18 @@ function hook_email_confirmed($node, $submission) {
  * React on an email confirmation request that has expired according
  * to the maximum request lifetime that the admin had set
  *
- * @param $node
- *   The node object of the webform for which an email confirmation
- *   request just expired
- *
- * @param $submission
- *   The submission object of the webform submission for which the
- *   email confirmation request just exirted
+ * @param $expired_submissions
+ *   An associative array of submissions that have expired. It's indexed
+ *   by nid's (node ID's), it's values are subarrays containing the sid's
+ *   (webform submission ID's) that have expired for this nid
  */
-function hook_confirmation_request_expired($node, $submission) {
-  // delete the webform submission that was not confirmed
-  webform_submission_delete($node, $submission);
+function hook_confirmation_request_expired($expired_submissions) {
+  // delete the webform submissions where the confirmation request
+  // have expired
+  foreach ($expired_submissions as $nid => $sids) {
+    $node = node_load($nid);
+    foreach ($sids as $sid) {
+      webform_submission_delete($node, webform_get_submission($nid, $sid));
+    }
+  }
 }
